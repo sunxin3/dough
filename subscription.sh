6aff55d3085a26f58fa7fa0b39e260afdbf78660
root <root@ubuntu.(none)>
2013-03-13 02:22:29 -0700

    subscription add

---
+++ subscription.sh
@@ -0,0 +1,3 @@
+#create subscribtion
+#dough-client -s user_id tenant_id resource_uuid resource_name region item item_type payment_type timestamp
+dough-client -s 0304d0005656403c9e0121adba5c0445 b356ed257f104ba998dce830ff644898 2a7dd614-5e34-43a0-bea9-0739cb64340b xxx "lenovo" instance m1.tiny hourlypp 2013-3-13
