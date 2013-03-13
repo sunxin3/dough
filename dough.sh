ad80c5ecca96d3bb7768421d843e3c1235d31af0
root <root@ubuntu.(none)>
2013-03-13 02:19:02 -0700

    dough add

--- dough.sh
+++ dough.sh
@@ -4,17 +4,20 @@ git clone https://github.com/sunxin3/dough.git
 cd /usr/lib/python2.6/site-packages/horizon/dashboards/nova
 mv dashboard.py dashboard.py.bak_geyg
 cp /root/nova/dashboard.py ./
+
 cp -rf /root/nova/dough ./
 
-cd /etc
-mkdir dough
-cd dough
-cp /root/dough.conf ./
+cd /usr/lib/python2.6/site-packages/horizon/dashboards/nova/templates/nova
+cp -rf /root/nova/templates/nova/dough ./
+
+service httpd restart
+
+
 
 cd /usr/lib/python2.6/site-packages
 mv dough-0.1.1-py2.6.egg dough-0.1.1-py2.6.egg.bak.geyg
 cp -rf /root/dough-0.1.1-py2.6.egg ./
 
-
-
-service httpd restart
+mkdir /etc/dough
+cd /etc/dough
+cp /root/dough.conf ./
