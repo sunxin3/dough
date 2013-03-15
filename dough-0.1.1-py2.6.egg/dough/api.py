9b9743a7dbc3d50dd48c4b58afe1f13c6ae52e4e
root <root@ubuntu.(none)>
2013-03-15 01:17:40 -0700

    update

--- dough-0.1.1-py2.6.egg/dough/api.py
+++ dough-0.1.1-py2.6.egg/dough/api.py
@@ -263,9 +263,10 @@ def query_monthly_report(context, timestamp_from=None,
             continue
         region_name = subscription['product']['region']['name']
         item_name = subscription['product']['item']['name']
-        resource_uuid = subscription['resource_uuid']
-        subscriptions.append([subscription_id, region_name, item_name, resource_uuid])
-    for subscription_id, region_name, item_name, resource_uuid in subscriptions:
+#        resource_uuid = subscription['resource_uuid']
+        resource_nm = subscription['resource_name']
+        subscriptions.append([subscription_id, region_name, item_name, resource_nm])
+    for subscription_id, region_name, item_name, resource_nm in subscriptions:
         purchases = db.purchase_get_all_by_subscription_and_timeframe(context,
                                                             subscription_id,
                                                             datetime_from,
@@ -273,9 +274,9 @@ def query_monthly_report(context, timestamp_from=None,
         if not purchases:
             continue
         monthly_report = dict()
-        monthly_report.setdefault('resourceuuid', 0)
+        monthly_report.setdefault('resourcenm', 0)
 #        monthly_report['regionnm'] = region_name
-        monthly_report['resourceuuid'] = resource_uuid
+        monthly_report['resourcenm'] = resource_nm
         monthly_report.setdefault('timefm', 0)
 #        monthly_report['timefm'] = timeframe
         monthly_report.setdefault('itemnm', 0)
@@ -297,6 +298,7 @@ def query_monthly_report(context, timestamp_from=None,
         
         monthly_report['timefm'] = timeframe
         monthly_report_list.append(monthly_report)
+    print monthly_report_list
     return {'data': monthly_report_list}
 
 def query_report(context, timestamp_from=None, timestamp_to=None,
